e46b72f9eebef1bd1613b9beeac75347668b1a66
comdet <listzone@hotmail.com>
2024-03-19 20:55:08 +0700

    update

---
+++ test_convert.py
@@ -0,0 +1,76 @@
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from data import *
+import torch.utils.data as data
+import numpy as np
+import cv2
+import tools
+import time
+import os.path as osp
+
+os.environ['PKG_CONFIG_PATH'] = ':/root/opencv-3.4.13/lib/pkgconfig'
+os.environ['LD_LIBRARY_PATH'] += ':/root/opencv-3.4.13/lib'
+os.environ['PATH'] += ':/root/opencv-3.4.13/bin'
+
+device = torch.device("cpu")
+
+input_size = [416, 416]
+
+print('test on custom ...')
+CUSTOM_CLASSES = ['face']
+class_names = ['face']
+class_indexs = None
+CUSTOM_ROOT = "./projects/kbai_zWVWG75/dataset"
+num_classes = len(CUSTOM_CLASSES)
+dataset = CustomDetection(root=CUSTOM_ROOT,labels=CUSTOM_CLASSES, image_sets=['val'], transform=None)
+conf_thresh = 0.1
+nms_thresh = 0.5
+trained_model= "projects/kbai_zWVWG75/output/best_map.pth"
+
+from models.slim_yolo_v2 import SlimYOLOv2 
+
+anchor_size = ANCHOR_SIZE
+net = SlimYOLOv2(device, 
+  input_size=input_size, 
+  num_classes=num_classes, 
+  conf_thresh=conf_thresh, 
+  nms_thresh=nms_thresh, 
+  anchor_size=anchor_size
+)
+
+
+net.load_state_dict(torch.load(trained_model, map_location=device))
+net.to(device).eval()
+print('Finished loading model!')
+
+# convert to onnx and ncnn
+from torchsummary import summary
+summary(net.to("cpu"), input_size=(3, input_size[0], input_size[1]), device="cpu")
+
+print("export model")
+net.no_post_process = True
+from convert import *
+onnx_out="out/yolov2.onnx"
+ncnn_out_param = "out/yolov2.param"
+ncnn_out_bin = "out/yolov2.bin"
+input_shape = (3, input_size[0], input_size[1])
+import os
+if not os.path.exists("out"):
+    os.makedirs("out")
+with torch.no_grad():
+    torch_to_onnx(net.to("cpu"), input_shape, onnx_out, device="cpu")
+    onnx_to_ncnn(input_shape, onnx=onnx_out, ncnn_param=ncnn_out_param, ncnn_bin=ncnn_out_bin)
+    print("convert end, ctrl-c to exit")
+net.no_post_process = False
+
+cmd = "tools/spnntools optimize out/yolov2.param out/yolov2.bin out/opt.param out/opt.bin"
+os.system(cmd)
+
+cmd2 = "tools/spnntools calibrate -p=out/opt.param -b=out/opt.bin -i=./data/custom/test_images -o=out/opt.table --m=127.5,127.5.0,127.5.0 --n=1.0,1.0,1.0 --size=224,224 -c -t=4"
+os.system(cmd2)
+
+cmd3 = "tools/spnntools quantize out/opt.param out/opt.bin out/opt_int8.param out/opt_int8.bin out/opt.table"
+os.system(cmd3)
